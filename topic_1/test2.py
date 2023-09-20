667291d2d9030d014aa7a7975483376bd526d0b9
DanyloKolesnykov <danilkolesnykov@gmail.com>
2023-09-20 20:39:36 +0300

    Dev test file for topic 01

---
+++ topic_1/test2.py
@@ -0,0 +1,21 @@
+text = "  BMW M5 e34 4.5L Diesel  "
+
+# Test the strip() method
+stripped_text = text.strip()  # Removes leading and trailing whitespace
+print(f"Strip: '{stripped_text}'")
+
+# Test the capitalize() method
+capitalized_text = text.capitalize()  # Capitalizes the first character
+print(f"Capitalize: '{capitalized_text}'")
+
+# Test the title() method
+title_text = text.title()  # Capitalizes the first letter of each word
+print(f"Title: '{title_text}'")
+
+# Test the upper() method
+upper_text = text.upper()  # Converts all characters to uppercase
+print(f"Upper: '{upper_text}'")
+
+# Test the lower() method
+lower_text = text.lower()  # Converts all characters to lowercase
+print(f"Lower: '{lower_text}'")
---
+++ topic_1/test3.py
@@ -0,0 +1,5 @@
+str = "DHKJB f hjbJbdfh bdubdf vfdw' vbdfpf vrgt785ui4gyoe fdhkjh sb4 gkiow4 rekwl"
+upper = str.count('D')
+lower = str.count('d')
+print(f"Uppercase 'D' count: {upper}")
+print(f"Lowercase 'd' count: {lower}")
