181e116c3ace57fd007d828deb06e99e178c9c7a
DanyloKolesnykov <danilkolesnykov@gmail.com>
2023-10-10 16:37:56 +0300

    Dev test file for topic 02

--- topic_02/task3.py
+++ topic_02/task3.py
@@ -1,5 +1,3 @@
-import math
-
 def add(x, y):
     return x + y
 
@@ -11,39 +9,25 @@ def multiply(x, y):
 
 def divide(x, y):
     if y == 0:
-        return "Ділення на нуль неможливе"
+        return "Помилка: Ділення на нуль"
     return x / y
 
-while True:
-    print("Виберіть операцію:")
-    print("1. Додавання")
-    print("2. Віднімання")
-    print("3. Множення")
-    print("4. Ділення")
-    print("5 Вихід")
+def calculator_match(value1, value2, operation):
+     match operation:
+        case '+': return add(value1, value2)
+         
+        case '-': return subtract(value1, value2)
+         
+        case '*': return multiply(value1, value2)
+         
+        case '/': return divide(value1, value2)
+         
+        case _: return "Помилка: Недійсна операція"
 
-    choice = input("Введіть номер операції (1/2/3/4/5): ")
-    
-    match choice:
-        case '1':
-            num1 = float(input("Введіть перше число: "))
-            num2 = float(input("Введіть друге число: "))
-            print("Результат:", add(num1, num2))
-        case '2':
-            num1 = float(input("Введіть перше число: "))
-            num2 = float(input("Введіть друге число: "))
-            print("Результат:", subtract(num1, num2))
-        case '3':
-            num1 = float(input("Введіть перше число: "))
-            num2 = float(input("Введіть друге число: "))
-            print("Результат:", multiply(num1, num2))
-        case '4':
-            num1 = float(input("Введіть перше число: "))
-            num2 = float(input("Введіть друге число: "))
-            result = divide(num1, num2)
-            print("Результат:", result)
-        case '5':
-            print("Бувай!")
-            break
-        case _:
-            print("Невірний вибір. Будь ласка спробуйте ще раз.")
+# Введення значень і операції користувача
+value1 = float(input("Введіть перше значення: "))
+value2 = float(input("Введіть друге значення: "))
+operation = input("Введіть операцію (+, -, *, /): ")
+
+result = calculator_match(value1, value2, operation)
+print(f"Результат: {result}")
