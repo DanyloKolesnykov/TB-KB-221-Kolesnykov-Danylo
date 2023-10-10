ca4d7d0dfea2a1b7e978043aa43d9edf0b68f85f
DanyloKolesnykov <danilkolesnykov@gmail.com>
2023-10-10 16:43:14 +0300

    Dev test file for topic 03

--- topic_03/task3.py
+++ topic_03/task3.py
@@ -1,42 +1,44 @@
-def test_dictionary_operations():
-    # Створення порожнього словника
-    my_dict = {}
+# Створюємо порожній словник
+my_dict = {}
 
-    # Додавання пар ключ-значення до словника
-    my_dict['name'] = 'John'
-    my_dict['age'] = 30
-    my_dict['city'] = 'New York'
-    my_dict['country'] = 'USA'
-    my_dict['job'] = 'Engineer'
-    my_dict['email'] = 'john@example.com'
+# Додаємо пари ключ-значення до словника
+my_dict['name'] = 'John'
+my_dict['age'] = 30
+my_dict['city'] = 'New York'
 
-    # Перевірка наявності ключів у словнику
-    assert 'name' in my_dict
-    assert 'city' in my_dict
+# Виводимо словник
+print("Початковий словник:")
+print(my_dict)
 
-    # Перевірка доступу до значень за ключами
-    assert my_dict['age'] == 30
-    assert my_dict['email'] == 'john@example.com'
+# Оновлюємо значення для ключа
+my_dict['age'] = 31
+print("Оновлений словник:")
+print(my_dict)
 
-    # Перевірка зміни значень за ключами
-    my_dict['age'] = 31
-    assert my_dict['age'] == 31
+# Видаляємо пару ключ-значення
+del my_dict['city']
+print("Словник після видалення ключа 'city':")
+print(my_dict)
 
-    # Перевірка видалення пар ключ-значення за ключем
-    del my_dict['job']
-    assert 'job' not in my_dict
+# Очищаємо весь словник
+my_dict.clear()
+print("Словник після очищення:")
+print(my_dict)
 
-    # Перевірка отримання списку всіх ключів та значень
-    keys = list(my_dict.keys())
-    values = list(my_dict.values())
-    assert keys == ['name', 'age', 'city', 'country', 'email']
-    assert values == ['John', 31, 'New York', 'USA', 'john@example.com']
+# Створюємо новий словник
+new_dict = {'name': 'Alice', 'age': 25, 'country': 'Canada'}
 
-    # Перевірка отримання пар ключ-значення у вигляді кортежів
-    items = list(my_dict.items())
-    assert items == [('name', 'John'), ('age', 31), ('city', 'New York'), ('country', 'USA'), ('email', 'john@example.com')]
+# Отримуємо ключі словника
+keys = new_dict.keys()
+print("Ключі словника:")
+print(keys)
 
-    print("Усі операції словників успішно протестовані!")
+# Отримуємо значення словника
+values = new_dict.values()
+print("Значення словника:")
+print(values)
 
-if __name__ == "__main__":
-    test_dictionary_operations()
+# Отримуємо пари ключ-значення словника
+items = new_dict.items()
+print("Пари ключ-значення словника:")
+print(items)
