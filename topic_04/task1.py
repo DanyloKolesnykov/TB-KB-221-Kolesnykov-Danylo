c7d30eae9acd3cc2b1484d1b281ff0f8c1a128e8
DanyloKolesnykov <danilkolesnykov@gmail.com>
2023-10-18 16:22:10 +0300

    Dev test file for topic 4

--- topic_04/task1.py
+++ topic_04/task1.py
@@ -1,3 +1,10 @@
+def genIntValue(prompt: str):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Помилка: Введіть ціле число.")
+
 def add(x, y):
     return x + y
 
@@ -14,34 +21,41 @@ def divide(x, y):
         return "Помилка: Ділення на нуль неможливе"
     return result
 
-while True:
-    print("Оберіть операцію:")
-    print("1. Додавання")
-    print("2. Віднімання")
-    print("3. Множення")
-    print("4. Ділення")
-    print("5. Вихід")
+def main():
+    while True:
+        print("Оберіть операцію:")
+        print("1. Додавання")
+        print("2. Віднімання")
+        print("3. Множення")
+        print("4. Ділення")
+        print("5. Вихід")
 
-    choice = input("Введіть номер операції (1/2/3/4/5): ")
+        choice = input("Введіть номер операції (1/2/3/4/5): ")
 
-    if choice == '5':
-        print("Дякую завикористання калькулятора!")
-        break
+        if choice == '5':
+            print("Дякую за використання калькулятора!")
+            break
 
-    if choice not in ['1', '2', '3', '4']:
-        print("Неправильний вибір операції. Спробуйте ще раз.")
-        continue
+        if choice not in ['1', '2', '3', '4']:
+            print("Неправильний вибір операції. Спробуйте ще раз.")
+            continue
 
-    num1 = float(input("Введіть перше число: "))
-    num2 = float(input("Введіть друге число: "))
+        num1 = genIntValue("Введіть перше число: ")
+        num2 = genIntValue("Введіть друге число: ")
 
-    if choice == '1':
-        result = add(num1, num2)
-    elif choice == '2':
-        result = subtract(num1, num2)
-    elif choice == '3':
-        result = multiply(num1, num2)
-    elif choice == '4':
-        result = divide(num1, num2)
+        if choice == '1':
+            result = add(num1, num2)
+        elif choice == '2':
+            result = subtract(num1, num2)
+        elif choice == '3':
+            result = multiply(num1, num2)
+        elif choice == '4':
+            if num2 == 0:
+                print("Помилка: Ділення на нуль неможливе")
+                continue
+            result = divide(num1, num2)
 
-    print(f"Результат: {result}")
+        print(f"Результат: {result}")
+
+if __name__ == "__main__":
+    main()
